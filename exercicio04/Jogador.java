0a8dfa79c9aa56d255accdbfe18a8e88cee1c754
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-03-31 15:33:24 -0300

    Update Jogador.java

--- exercicio04/Jogador.java
+++ exercicio04/Jogador.java
@@ -2,15 +2,12 @@ public class Jogador {
     private String nome;
     private int pontuacao;
     private int nivel;
-
-    // Construtor
+    
     public Jogador(String nome, int pontuacaoInicial, int nivelInicial) {
         this.nome = nome;
         this.pontuacao = pontuacaoInicial;
         this.nivel = nivelInicial;
     }
-
-    // Método para aumentar a pontuação do jogador
     public void aumentarPontuacao(int pontos) {
         if (pontos > 0) {
             pontuacao += pontos;
@@ -19,21 +16,15 @@ public class Jogador {
             System.out.println("Valor de pontuação inválido.");
         }
     }
-
-    // Método para subir de nível do jogador
     public void subirNivel() {
         nivel++;
         System.out.println("Jogador subiu para o nível " + nivel);
     }
-
-    // Método para exibir as informações do jogador
     public void exibirInformacoes() {
         System.out.println("Nome do jogador: " + nome);
         System.out.println("Pontuação: " + pontuacao);
         System.out.println("Nível: " + nivel);
     }
-
-    // Métodos para obter o nome, pontuação e nível do jogador
     public String getNome() {
         return nome;
     }
