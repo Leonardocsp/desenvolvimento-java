91209061fcf3f0ea66986743008b62346e265cfc
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-03-31 15:45:55 -0300

    Update Contato.java

--- exercicio06/Contato.java
+++ exercicio06/Contato.java
@@ -1,23 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-
-// Classe Contato
-class Contato {
+public class Contato {
+    
     private String nome;
     private String telefone;
 
-    // Construtor
     public Contato(String nome, String telefone) {
         this.nome = nome;
         this.telefone = telefone;
     }
-
-    // Métodos para acessar os atributos privados
     public String getNome() {
         return nome;
     }
-
     public String getTelefone() {
         return telefone;
+    }
+    public String toString() {
+        return "Nome: " + nome + ", Telefone: " + telefone;
     }
 }
