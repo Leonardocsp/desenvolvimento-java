2fa7996b1201cd856198a329d33d3455e36ecbfd
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-03-31 14:53:26 -0300

    Update ContaBancaria.java

--- exercicio03/ContaBancaria.java
+++ exercicio03/ContaBancaria.java
@@ -3,14 +3,11 @@ public class ContaBancaria {
     private String nomeTitular;
     private double saldo;
 
-    // Construtor
     public ContaBancaria(int numeroConta, String nomeTitular) {
         this.numeroConta = numeroConta;
         this.nomeTitular = nomeTitular;
         this.saldo = 0.0; // saldo inicial zero
     }
-
-    // Método para depositar um valor na conta
     public void depositar(double valor) {
         if (valor > 0) {
             saldo += valor;
@@ -19,8 +16,6 @@ public class ContaBancaria {
             System.out.println("Valor de depósito inválido.");
         }
     }
-
-    // Método para sacar um valor da conta
     public void sacar(double valor) {
         if (valor > 0 && valor <= saldo) {
             saldo -= valor;
@@ -29,18 +24,12 @@ public class ContaBancaria {
             System.out.println("Saldo insuficiente ou valor de saque inválido.");
         }
     }
-
-    // Método para retornar o saldo atual da conta
     public double getSaldo() {
         return saldo;
     }
-
-    // Método para retornar o número da conta
     public int getNumeroConta() {
         return numeroConta;
     }
-
-    // Método para retornar o nome do titular da conta
     public String getNomeTitular() {
         return nomeTitular;
     }
