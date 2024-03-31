e6869af972574f4a628aa6405608160ab8752c7e
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-03-31 15:36:11 -0300

    Update Main.java

--- exercicio05/Main.java
+++ exercicio05/Main.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        // Exemplo de uso da classe Carro
+      
         Carro meuCarro = new Carro("Toyota", "Corolla", 2022, "ABC1234");
         meuCarro.exibirInformacoes();
-
         meuCarro.acelerar(50);
         meuCarro.frear(20);
-
         meuCarro.exibirInformacoes();
     }
 }
