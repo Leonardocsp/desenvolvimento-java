319725de567004f3149c1ee238639fb59de2fcb2
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-03-31 14:51:59 -0300

    Add files via upload

---
+++ exercicio03/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        // Exemplo de uso da classe ContaBancaria
+        ContaBancaria minhaConta = new ContaBancaria(123456, "Fulano de Tal");
+        System.out.println("Saldo inicial: " + minhaConta.getSaldo());
+
+        minhaConta.depositar(1000);
+        System.out.println("Saldo após depósito: " + minhaConta.getSaldo());
+
+        minhaConta.sacar(500);
+        System.out.println("Saldo após saque: " + minhaConta.getSaldo());
+
+        minhaConta.sacar(600); // Isso deve falhar, saldo insuficiente
+        System.out.println("Saldo final: " + minhaConta.getSaldo());
+    }
+}
