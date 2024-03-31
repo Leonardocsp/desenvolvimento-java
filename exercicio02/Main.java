4a7e5b8537eb399014bd1080d81b9ee5296a1189
Leonardocsp <147525997+Leonardocsp@users.noreply.github.com>
2024-03-31 14:50:08 -0300

    Update Main.java

--- exercicio02/Main.java
+++ exercicio02/Main.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class Main
 {
-    // Método principal para teste
     public static void main(String[] args) {
-        // Criando uma instância de Livro
         Livro livro = new Livro("Dom Quixote", "Miguel de Cervantes", 1605);
-        
-        // Exibindo as informações do livro
         livro.exibirInfo();
     }
 }
